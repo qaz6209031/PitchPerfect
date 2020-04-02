62486b0a3fa9492968f13a3b5c65859258d0ec2c
Kai-Chin Huang <qaz6209031@gmail.com>
2020-04-02 16:19:01 +0800

    Finish PlaySoundsViewController UI

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class PlaySoundsViewController: UIViewController {
     
-    var recordAudioURL: URL!
+    var recordedAudioURL : URL!
 
     override func viewDidLoad() {
         super.viewDidLoad()
